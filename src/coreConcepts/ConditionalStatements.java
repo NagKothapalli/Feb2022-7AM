8896127b3d1857b1cd90e0d52adfc41a6a991be8
NagKothapalli <nag022@gmail.com>
2022-02-25 07:19:02 +0530

    added first program with if condition

--- src/coreConcepts/ConditionalStatements.java
+++ src/coreConcepts/ConditionalStatements.java
@@ -5,5 +5,6 @@ public class ConditionalStatements
 	//https://github.com/NagKothapalli/Feb2022-7AM.git
 	//Decision making statements
 	//if , if else , else if , nested if , switch case
+	//write a program to read two integers as input and compare the numbers
 
 }
