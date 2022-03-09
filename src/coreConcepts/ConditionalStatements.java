a9f16533b95890763044f9630a49c27903704c96
NagKothapalli <nag022@gmail.com>
2022-03-09 07:02:59 +0530

    March8th code pushed

--- src/coreConcepts/ConditionalStatements.java
+++ src/coreConcepts/ConditionalStatements.java
@@ -44,12 +44,168 @@ public class ConditionalStatements
 			System.out.println("Both A and B are not equal");
 		}
 	}
+	
+	//write a program to read two integers as input and perform sum them if equal if not do subtraction
+	@Test
+	public void decisionMaking_unitTest() //TDD : Test Driven Development
+	{
+		decisionMaking(55,55); // use case / test case  5 / 10 = 50 %
+		decisionMaking(545,55);
+	}
+	public void decisionMaking(int a,int b)
+	{
+		if(a == b) //22==22 -> true  22==42  -> false
+		{
+			//true
+			int sum = a+b;
+			System.out.println("Sum of two numbers :" + sum);
+		}
+		else
+		{
+			int diff = a-b;
+			System.out.println("Difference of two numbers :" + diff);
+		}
+		
+		System.out.println("just continue");
+		
+	}
+	//write a program to read two integers as input and perform sum them if equal 
+	//if a > b do subtraction  , do multiplication a < b
+	@Test
+	public void workWithElseif_unitTest()
+	{
+		workWithElseif(44,44); //33 %   5/15 *100 = 33.333
+		workWithElseif(66,66); //duplicate test case
+		workWithElseif(66,26); //33%
+		workWithElseif(66,126);
+	}
+	public void workWithElseif(int a,int b) //dev code - implementation
+	{
+		System.out.println("A  value :" + a);
+		System.out.println("B value :"+ b);
+		if(a==b)
+		{
+			int sum = a+b;
+			System.out.println("Sum of two numbers :" + sum);
+		}
+		else if(a > b)
+		{
+			int diff = a-b;
+			System.out.println("Difference of two numbers :" + diff);
+		}
+		else if(a<b)
+		{
+			int prod = a*b;
+			System.out.println("Product of two numbers :" + prod);
+		}
+	}
+	//write a program to read two integers as input and perform the below task if and only if first number is above 10
+	//Task : sum them if equal if a > b do subtraction  , do multiplication a < b
+	public void nestedIfCondition(int a,int b)
+	{
+		System.out.println("Your A value :"+a);
+		System.out.println("Your B value :"+b);
+		//Algorithm -> Flowchart -> Program
+		if(a>10) //6 - false 24 ->true  // condition -1 
+		{
+			if(a==b)  //condition-2  Nested if condition
+			{
+				int sum = a+b;
+				System.out.println("Sum of numbers :" + sum);
+			}
+			else if(a>b)
+			{
+				int diff = a-b;
+				System.out.println("Difference of two numbers :" + diff);
+			}
+			else if(a<b)
+			{
+				int prod = a*b;
+				System.out.println("Product of two numbers :" + prod);
+			}
+		}
+		else
+		{
+			System.out.println("Your A value is not above 10");
+		}
+	}
+	@Test
+	public void nestedIfCondition_test()
+	{
+		nestedIfCondition(8,18);
+		nestedIfCondition(18,12);
+		nestedIfCondition(18,132);
+		nestedIfCondition(18,18);
+		nestedIfCondition(8,12);
+	}
+	//write a program to read a number and find that number is even or odd.
+	@Test
+	public void findEvenOdd_test()
+	{
+		findEvenOdd(345);
+		findEvenOdd(244);
+	}
+	public void findEvenOdd(int num)  // num / 2 = Q    num % 2  = R   if R == 0 then even
+	{
+		System.out.println("Given Number :" + num);
+		int rem = num % 2 ;
+		if(rem == 0)
+		{
+			System.out.println("Given Number is EVEN");
+		}
+		else
+		{
+			System.out.println("Given Number is ODD");
+		}
+	}
+	//write a program to read two integers as input and perform the below task if and only if both are above 10
+		//Task : sum them if equal if a > b do subtraction  , do multiplication a < b
+	
+	public void logicalOperator(int a,int b)
+	{
+		System.out.println("Your A value :" + a);
+		System.out.println("Ypur B value :" + b);
+		if((a>10) && (b>10))  // T  and T : T   // && - logical operator
+		{
+			if(a==b)  //condition-2  Nested if condition
+			{
+				int sum = a+b;
+				System.out.println("Sum of numbers :" + sum);
+			}
+			else if(a>b)
+			{
+				int diff = a-b;
+				System.out.println("Difference of two numbers :" + diff);
+			}
+			else if(a<b)
+			{
+				int prod = a*b;
+				System.out.println("Product of two numbers :" + prod);
+			}
+		}
+		else
+		{
+			System.out.println("Both might be below 10 , any one of them might be below 10");
+		}
+	}
+	@Test
+	public void logicalOperator_test()
+	{
+		logicalOperator(7,4);
+		logicalOperator(7,14);
+		logicalOperator(17,4);
+		logicalOperator(17,17);
+		logicalOperator(17,14);
+		logicalOperator(17,24);
+		
+	}
 	@Test
 	public void compareNumbers_ifelse_unitTest()   //Unit Testing
 	{
 		compareNumbers_ifelse(45,88);
 		compareNumbers_ifelse(45,45);
 	}
+	
 	
 	public void compareNumbers_ifelse(int a,int b) //Dev code
 	{		
---
+++ src/coreConcepts/LoopingStatements.java
@@ -0,0 +1,115 @@
+package coreConcepts;
+
+import org.junit.Test;
+
+public class LoopingStatements
+{
+	@Test
+	public void printTenNumbers()
+	{
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");		
+	}
+	@Test
+	public void applyColor()  //starting point , how long , step size
+	{
+		System.out.println("Apply color to step 1");
+		System.out.println("Apply color to step 2");
+		System.out.println("Apply color to step 3");
+		System.out.println("Apply color to step 4"); //...................
+	}
+	////starting point , how long , step size
+	// for(iterator = 1 ; condition ; step size )  {    Task    } 
+	@Test
+	public void applyColorToSteps()
+	{
+		for(int iterator=1; iterator<=50 ; iterator=iterator+1)  // 1<50 2<50 ...49<50 50<=50
+		{
+			//Task
+			System.out.println("Apply Color to Step:" + iterator);
+		}
+	}
+	
+	@Test
+	public void applyRedColorToOddSteps()
+	{
+		for(int iterator=1; iterator<=50 ; iterator=iterator+2)  // 1<50 2<50 ...49<50 50<=50
+		{
+			//Task
+			System.out.println("Apply Color to Odd Steps:" + iterator);
+		}
+	}
+	
+	@Test
+	public void applyGreenColorEvenSteps()
+	{
+		for(int iterator=2; iterator<=50 ; iterator=iterator+2)  // 1<50 2<50 ...49<50 50<=50
+		{
+			//Task
+			System.out.println("Apply Color to Even Steps:" + iterator);
+		}
+	}
+	
+	@Test
+	public void applyGreenColorEvenSteps_if()
+	{
+		for(int i=1; i<=50 ; i++)  // 1<50 2<50 ...49<50 50<=50
+		{
+			//Task
+			int rem = i % 2;
+			if(rem == 0)
+			{
+				System.out.println("Apply Green Color to Even Steps:" + i);
+			}
+			else
+			{
+				System.out.println("Apply RED Color to Even Steps:" + i);
+			}
+			
+		}
+	}
+	
+	@Test
+	public void applyRedColorToOddSteps_if()
+	{
+		for(int i=1; i<=50 ; i++)  // 1<50 2<50 ...49<50 50<=50
+		{
+			//Task
+			int rem = i % 2;
+			if(!(rem == 0))
+			{
+				System.out.println("Apply Color to Odd Steps:" + i);
+			}
+			
+		}
+	}
+	
+	//Write a program to find a student is there in the class .
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
